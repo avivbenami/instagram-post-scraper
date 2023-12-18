d3432b12b95aa81ed9ef8ecc70be1ebce7fa58a8
Aviv Ben-Ami <avivbenami@gmail.com>
2023-12-18 20:26:32 +0200

    Adding annotation

--- instagram.py
+++ instagram.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 class InstagramPostScraper:
-    def __init__(self, post_url: list) -> None:
+    def __init__(self, post_url: str) -> None:
         self.post_url = post_url
         self.driver = self._create_driver()
 
